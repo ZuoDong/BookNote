905384b0589fb9df7967a8998529fa5bf3a57bc0
zuodong <zuodong@gamil.com>
2017-01-05 22:58:53 +0800

    接口隔离原则——16

---
+++ app/src/main/java/com/zuodong/androiddesignpatterns/Content/ChapterOne/CloseUtils.java
@@ -0,0 +1,32 @@
+package com.zuodong.androiddesignpatterns.Content.ChapterOne;
+
+import java.io.Closeable;
+import java.io.IOException;
+
+/**
+ * Created by dongdong on 2017/1/5.
+ *
+ * 接口隔离原则-16
+ *
+ * 客户端不应该依赖它不需要的接口
+ * 目的是系统解开耦合，从而容易重构，更改，和重新部署
+ */
+
+public class CloseUtils {
+
+    public CloseUtils() {}
+
+    /**
+     * 关闭Closeable对象
+     * @param closeable
+     */
+    public static void closeQuietly(Closeable closeable){
+        if(null != closeable){
+            try {
+                closeable.close();
+            }catch (IOException e){
+                e.printStackTrace();
+            }
+        }
+    }
+}
